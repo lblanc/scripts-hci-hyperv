8a6a8f3145f2f1d3f2808a76649634235ceecd44
Luc <email@luc-blanc.com>
2020-04-07 17:11:51 +0200

    scripts hci hyperv 20200407

---
+++ Set-IP-vEthernet_Adapters.ps1
@@ -0,0 +1,45 @@
+﻿#### SCRIPT START #####
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+
+$adapters = get-NetAdapter | where {$_.Name -imatch "vEthernet "}
+$mask =24
+$IPType = "IPv4"
+
+function Select-Folder($message='Select a folder', $path = 0) { 
+    $object = New-Object -comObject Shell.Application  
+     
+    $folder = $object.BrowseForFolder(0, $message, 0, $path) 
+    if ($folder -ne $null) { 
+        $folder.self.Path 
+    } 
+} 
+$exportpath = Select-Folder 'Select the folder to export ip dump files' 
+
+Get-NetIPAddress -AddressFamily IPv4 | where {$_.InterfaceAlias -imatch "vEthernet "}  | Export-Csv -Path $exportpath"\Old_iSCSI_IP.csv"
+
+foreach ($adapter in $adapters)
+{
+# Remove any existing IP, gateway from our ipv4 adapter
+If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+    $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
+}
+
+If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+    $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
+}
+$ip = [Microsoft.VisualBasic.Interaction]::InputBox("Enter IP for this adapter", $adapter.name, $ip)
+$mask  = [Microsoft.VisualBasic.Interaction]::InputBox("Enter mask for this adapter", $adapter.name, $mask)
+
+
+# Configure the IP address and default gateway
+$adapter | New-NetIPAddress `
+    -AddressFamily $IPType `
+    -IPAddress $ip `
+    -PrefixLength $mask `
+}
+
+Get-NetIPAddress -AddressFamily IPv4 | where {$_.InterfaceAlias -imatch "vEthernet "}  | Export-Csv -Path $exportpath"\New_iSCSI_IP.csv"
+
+
+#### SCRIPT END #####
---
+++ vSwitch Setup.ps1
@@ -0,0 +1,58 @@
+# The NICs facing the customer network
+$prodNicName1 = "Prod1"
+$prodNicName2 = "Prod2"
+# The NICs used for the iSCSI traffic
+$iSCSINicName1 = "iSCSI1"
+$iSCSINicName2 = "iSCSI2"
+
+## No need to change these variables
+$productionTeamName = "Production"
+$productionvSwitchName = "Production_vSwitch"
+$iSCSI01vSwitchName = "iSCSI-01_vSwitch"
+$iSCSI02vSwitchName = "iSCSI-02_vSwitch"
+
+
+
+### Create the necessary teams
+## The Production Team (Active / Active, but switch independent)
+New-NetLbfoTeam -Name "$productionTeamName" -TeamMembers "$prodNicName1","$prodNicName2" -TeamingMode SwitchIndependent -LoadBalancingAlgorithm HyperVPort -confirm:$false
+
+
+### Create the vSwitches. It is important to NOT use "allowmanagementos" here. Also this is where you need to set the bandwidthmode. If not done, this can´t be changed later!
+
+
+## Production vSwitch
+New-VMSwitch -Name "$productionvSwitchName" -NetAdapterName "$productionTeamName" -AllowManagementOS $false -MinimumBandwidthMode weight -notes "via Ethernet Adapter '$productionTeamName' (netlbfo team)"
+
+## iSCSI vSwitch 1
+New-VMSwitch -Name "$iSCSI01vSwitchName" -NetAdapterName "$iSCSINicName1" -AllowManagementOS $false -MinimumBandwidthMode weight -notes "via Ethernet Adapter '$iSCSINicName1'"
+## iSCSI vSwitch 2
+New-VMSwitch -Name "$iSCSI02vSwitchName" -NetAdapterName "$iSCSINicName2" -AllowManagementOS $false -MinimumBandwidthMode weight -notes "via Ethernet Adapter '$iSCSINicName2'"
+
+
+### Create virtual NICs
+## on the production vSwitch
+Add-VMNetworkAdapter -Name "CSV" -ManagementOS -SwitchName "$productionvSwitchName"
+Add-VMNetworkAdapter -Name "Heartbeat" -ManagementOS -SwitchName "$productionvSwitchName"
+Add-VMNetworkAdapter -Name "LiveMigration" -ManagementOS -SwitchName "$productionvSwitchName"
+## on iSCSI vSwitch 01
+Add-VMNetworkAdapter -Name "iSCSI-FE-01" -ManagementOS -SwitchName "$iSCSI01vSwitchName"
+Add-VMNetworkAdapter -Name "iSCSI-MR-01" -ManagementOS -SwitchName "$iSCSI01vSwitchName"
+## on iSCSI vSwitch 02
+Add-VMNetworkAdapter -Name "iSCSI-FE-02" -ManagementOS -SwitchName "$iSCSI02vSwitchName"
+Add-VMNetworkAdapter -Name "iSCSI-MR-02" -ManagementOS -SwitchName "$iSCSI02vSwitchName"
+
+
+
+### Assign the bandwidth settings
+## Ensure that the Management Network has always priority to keep control over the cluster
+Set-VMNetworkAdapter -ManagementOS -Name "Heartbeat" -MinimumBandwidthWeight 100
+Set-VMNetworkAdapter -ManagementOS -Name "CSV" -MinimumBandwidthWeight 90
+Set-VMNetworkAdapter -ManagementOS -Name "LiveMigration" -MinimumBandwidthWeight 20
+## iSCSI vSwitch 1
+Set-VMNetworkAdapter -ManagementOS -Name "iSCSI-MR-01" -MinimumBandwidthWeight 100
+Set-VMNetworkAdapter -ManagementOS -Name "iSCSI-FE-01" -MinimumBandwidthWeight 80
+## iSCSI vSwitch 2
+Set-VMNetworkAdapter -ManagementOS -Name "iSCSI-MR-02" -MinimumBandwidthWeight 100
+Set-VMNetworkAdapter -ManagementOS -Name "iSCSI-FE-02" -MinimumBandwidthWeight 80
+
